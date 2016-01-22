61f6c14bee9cc59801d2bd40279b050ee931f293
Christoph Lukas <lukas@pingworks.de>
2016-01-22 11:59:49 +0100

    bump version

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,6 @@ maintainer_email 'info@pingworks.de'
 license          'Apache 2.0'
 description      'Installs and configures meetup'
 long_description 'Installs and configures meetup'
-version          '0.1.0'
+version          '0.1.1'
 
 depends 'pw_base'
