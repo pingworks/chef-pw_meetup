1a70e5e840891d9f8c3e6f26d76d81c37c75351f
Christoph Lukas <lukas@pingworks.de>
2016-01-22 10:47:54 +0100

    Metadata

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name             'pw_meetup'
+maintainer       'cluk'
+maintainer_email 'info@pingworks.de'
+license          'Apache 2.0'
+description      'Installs and configures meetup'
+long_description 'Installs and configures meetup'
+version          '0.1.0'
